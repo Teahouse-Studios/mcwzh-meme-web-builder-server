ffd1ea9cf7f34a6548a29bbee9dfe6766260885c
LittleC <i@ltlec.cn>
2020-12-31 21:22:32 +0800

    :zap: for proxy

--- online_builder.py
+++ online_builder.py
@@ -153,4 +153,4 @@ if __name__ == '__main__':
             asyncio.set_event_loop(
                 asyncio.ProactorEventLoop()
             )
-    web.run_app(app, host="127.0.0.1", port=8000)
+    web.run_app(app, host="0.0.0.0", port=8000)
